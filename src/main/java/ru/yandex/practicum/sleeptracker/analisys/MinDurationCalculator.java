977835ca5c4016d59949e28f18d11b9b12cb5fb9
Vladislav <vladislaw.sirotkin@yandex.ru>
2026-03-25 20:45:20 +0300

    Задание спринта 8

---
+++ src/main/java/ru/yandex/practicum/sleeptracker/analisys/MinDurationCalculator.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.sleeptracker.analisys;
+
+import ru.yandex.practicum.sleeptracker.SleepAnalysisResult;
+import ru.yandex.practicum.sleeptracker.SleepingSession;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class MinDurationCalculator implements Function<List<SleepingSession>, SleepAnalysisResult> {
+
+    @Override
+    public SleepAnalysisResult apply(List<SleepingSession> sessions) {
+        long min = sessions.stream()
+                .mapToLong(SleepingSession::getDuration)
+                .min()
+                .orElse(0);
+        return new SleepAnalysisResult("Минимальная продолжительность сна (мин)", min);
+    }
+}
